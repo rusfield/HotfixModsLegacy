ad16c863206906cce081bea97dca1f4b9016c98c
Marius <marius@fjeld.et>
2022-08-16 13:46:44 +0200

    Sound preparations

---
+++ HotfixMods.Infrastructure/DtoModels/SoundDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotfixMods.Infrastructure.DtoModels
+{
+    public class SoundDto : Dto
+    {
+        public double? PitchVariationPlus { get; set; }
+        public double? PitchVariationMinus { get; set; }
+        public double? PitchAdjust { get; set; }
+        public List<int> FileDataIds { get; set; }
+    }
+}
---
+++ HotfixMods.Infrastructure/Services/SoundService.cs
@@ -0,0 +1,16 @@
+﻿using HotfixMods.Core.Providers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotfixMods.Infrastructure.Services
+{
+    public partial class SoundService : Service
+    {
+        public SoundService(IDb2Provider db2Provider, IMySqlProvider mySqlProvider) : base(db2Provider, mySqlProvider)  {   }
+
+
+    }
+}

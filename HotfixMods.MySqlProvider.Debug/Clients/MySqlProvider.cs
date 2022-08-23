a0010f4f6e2da8e117889761fad151124db0d9f7
Marius <marius@fjeld.et>
2022-08-23 09:30:47 +0200

    Add dummy MySQL Provider

---
+++ HotfixMods.MySqlProvider.Debug/Clients/MySqlProvider.cs
@@ -0,0 +1,83 @@
+﻿using HotfixMods.Core.Models.Interfaces;
+using HotfixMods.Core.Providers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotfixMods.MySqlProvider.Debug.Clients
+{
+    public class MySqlProvider : IMySqlProvider
+    {
+        public async Task<bool> CharactersConnectionTestAsync()
+        {
+            return true;
+        }
+
+        public async Task<bool> HotfixesConnectionTestAsync()
+        {
+            return true;
+        }
+
+        public async Task<bool> WorldConnectionTestAsync()
+        {
+            return true;
+        }
+
+        public async Task AddAsync<T>(T entity)
+            where T : class, ITrinityCore
+        {
+            return;
+        }
+
+        public async Task AddManyAsync<T>(IEnumerable<T> entities)
+            where T : class, ITrinityCore
+        {
+            return;
+        }
+
+        public async Task DeleteAsync<T>(T entity)
+            where T : class, ITrinityCore
+        {
+            return;
+        }
+
+        public async Task DeleteManyAsync<T>(IEnumerable<T> entities)
+            where T : class, ITrinityCore
+        {
+            return;
+        }
+
+        public async Task<T?> GetAsync<T>(Expression<Func<T, bool>> predicate)
+            where T : class, ITrinityCore
+        {
+            return (T)Activator.CreateInstance(typeof(T));
+        }
+
+        public async Task<IEnumerable<T>> GetManyAsync<T>(Expression<Func<T, bool>> predicate)
+            where T : class, ITrinityCore
+        {
+            return new List<T>() { (T)Activator.CreateInstance(typeof(T)) };
+        }
+
+        public async Task<bool> TableExists<T>()
+            where T : class, ITrinityCore
+        {
+            return true;
+        }
+
+        public async Task UpdateAsync<T>(T entity)
+            where T : class, ITrinityCore
+        {
+            return;
+        }
+
+        public async Task UpdateManyAsync<T>(IEnumerable<T> entities)
+            where T : class, ITrinityCore
+        {
+            return;
+        }
+    }
+}

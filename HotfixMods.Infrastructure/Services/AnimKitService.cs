0a4e223628e4740ab6a0289ab91896a5fb464a81
Marius <marius@fjeld.et>
2022-08-24 12:31:01 +0200

    Save and Delete AnimKit

--- HotfixMods.Infrastructure/Services/AnimKitService.cs
+++ HotfixMods.Infrastructure/Services/AnimKitService.cs
@@ -1,4 +1,5 @@
-﻿using HotfixMods.Core.Models;
+﻿using HotfixMods.Core.Enums;
+using HotfixMods.Core.Models;
 using HotfixMods.Core.Providers;
 using HotfixMods.Infrastructure.Dashboard;
 using HotfixMods.Infrastructure.DtoModels;
@@ -36,12 +37,38 @@ namespace HotfixMods.Infrastructure.Services
 
         public async Task DeleteAsync(int id)
         {
-
+            await DeleteFromHotfixesAsync(id);
         }
 
         public async Task SaveAsync(AnimKitDto animKitDto)
         {
+            if (animKitDto.Segments.Count > 20)
+            {
+                throw new Exception($"AnimKit should not have more than 50 Segments.");
+            }
 
+
+            var hotfixId = await GetNextHotfixIdAsync();
+            animKitDto.InitHotfixes(hotfixId, VerifiedBuild);
+
+            if (animKitDto.IsUpdate)
+            {
+                await _mySql.UpdateAsync(BuildHotfixModsData(animKitDto));
+                await _mySql.UpdateAsync(BuildAnimKit(animKitDto));
+
+                var segments = await _mySql.GetManyAsync<AnimKitSegment>(c => c.ParentAnimKitId == animKitDto.Id);
+                if (segments.Any())
+                    await _mySql.DeleteManyAsync(segments);
+                await _mySql.AddManyAsync(BuildAnimKitSegment(animKitDto));
+            }
+            else
+            {
+                await _mySql.AddAsync(BuildHotfixModsData(animKitDto));
+                await _mySql.AddAsync(BuildAnimKit(animKitDto));
+                await _mySql.AddManyAsync(BuildAnimKitSegment(animKitDto));
+            }
+
+            await AddHotfixes(animKitDto.GetHotfixes());
         }
 
         public async Task<List<AnimKitDto>> GetAnimKitsByIdAsync(int animKitId, Action<string, string, int>? progressCallback = null)
@@ -88,6 +115,32 @@ namespace HotfixMods.Infrastructure.Services
             }
 
             return new() { result };
+        }
+
+        async Task DeleteFromHotfixesAsync(int id)
+        {
+            var animKit = await _mySql.GetAsync<AnimKit>(s => s.Id == id);
+            var animKitSegments = await _mySql.GetManyAsync<AnimKitSegment>(s => s.ParentAnimKitId == id);
+            var hotfixModsData = await _mySql.GetAsync<HotfixModsData>(h => h.Id == id);
+
+            if (null != animKit)
+                await _mySql.DeleteAsync(animKit);
+
+            if (animKitSegments.Any())
+                await _mySql.DeleteManyAsync(animKitSegments);
+
+            var hotfixData = await _mySql.GetManyAsync<HotfixData>(h => h.UniqueId == id);
+            if (hotfixData != null && hotfixData.Count() > 0)
+            {
+                foreach (var hotfix in hotfixData)
+                {
+                    hotfix.Status = HotfixStatuses.INVALID;
+                }
+                await _mySql.UpdateManyAsync(hotfixData);
+            }
+
+            if (null != hotfixModsData)
+                await _mySql.DeleteAsync(hotfixModsData);
         }
     }
 }

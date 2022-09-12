1bad4b6b2d7c57ab3c8a523a3290b5ed7a31914a
Marius <marius@fjeld.et>
2022-09-12 13:40:16 +0200

    GameObjectDisplayInfo

---
+++ HotfixMods.Core/Models/GameObjectDisplayInfo.cs
@@ -0,0 +1,21 @@
+﻿using HotfixMods.Core.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotfixMods.Core.Models
+{
+    public class GameObjectDisplayInfo : IDb2, IHotfixesSchema
+    {
+        public int Id { get; set; }
+        public decimal GeoBox0 { get; set; }
+        public decimal GeoBox1 { get; set; }
+        public decimal Geobox2 { get; set; }
+        public decimal Geobox3 { get; set; }
+        public decimal Geobox4 { get; set; }
+        public decimal Geobox5 { get; set; }
+        public int FileDataId { get; set; }
+    }
+}

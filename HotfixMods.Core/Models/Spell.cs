6bd3b50edc60c136590fcbe9633319640cb0ca9b
Marius <marius@fjeld.et>
2022-09-12 13:20:35 +0200

    Add some spell files

---
+++ HotfixMods.Core/Models/Spell.cs
@@ -0,0 +1,16 @@
+﻿using HotfixMods.Core.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotfixMods.Core.Models
+{
+    public class Spell : IDb2, IHotfixesSchema
+    {
+        public int Id { get; set; }
+        public string NameSubtext { get; set; }
+        public string AuraDescription { get; set; }
+    }
+}
---
+++ HotfixMods.Core/Models/SpellEffect.cs
@@ -0,0 +1,19 @@
+﻿using HotfixMods.Core.Enums;
+using HotfixMods.Core.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotfixMods.Core.Models
+{
+    public class SpellEffect : IDb2, IHotfixesSchema
+    {
+        public int Id { get; set; }
+        public SpellEffectAuras EffectAura { get; set; }
+        public int EffectIndex { get; set; }
+        public SpellEffects Effect { get; set; }
+        public int SpellId { get; set; }
+    }
+}

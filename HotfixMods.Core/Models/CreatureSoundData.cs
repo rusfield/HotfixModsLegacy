bade74a0791a9a8940236b90aeab85cd2885b6da
Marius <marius@fjeld.et>
2022-09-16 15:20:35 +0200

    Add Creature Sound Data

---
+++ HotfixMods.Core/Models/CreatureSoundData.cs
@@ -0,0 +1,19 @@
+﻿using HotfixMods.Core.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotfixMods.Core.Models
+{
+    public class CreatureSoundData : IDb2, IHotfixesSchema
+    {
+        public int Id { get; set; }
+        public int SoundExertionId { get; set; }
+        public int SoundExertionCriticalId { get; set; }
+        public int SoundInjuryId { get; set; }
+        public int SoundInjuryCriticalId { get; set; }
+        public int SoundDeathId { get; set; }
+    }
+}

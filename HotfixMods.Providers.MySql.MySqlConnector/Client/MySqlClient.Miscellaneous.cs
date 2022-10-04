5c643d655b28a70d6cb22b9b44cab6602ddf74d2
Marius <marius@fjeld.et>
2022-10-04 21:57:56 +0200

    Fixes for new MySql client

---
+++ HotfixMods.Providers.MySql.MySqlConnector/Client/MySqlClient.Miscellaneous.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace HotfixMods.Providers.MySql.MySqlConnector.Client
+{
+    public partial class MySqlClient
+    {
+        // Simple SQL injection protection,
+        // remove if needed.
+        void ValidateInput(params string?[] inputs)
+        {
+            var regex = new Regex("^[A-Za-z0-9 ='_-]*$");
+            foreach (var input in inputs)
+            {
+                if (!string.IsNullOrEmpty(input))
+                {
+                    if (!regex.Match(input).Success)
+                        throw new Exception($"'{input}' is not a valid input.");
+                }
+            }
+        }
+    }
+}
--- HotfixMods.Providers.MySql.MySqlConnector/Client/MySqlClient.cs
+++ HotfixMods.Providers.MySql.MySqlConnector/Client/MySqlClient.cs
@@ -2,66 +2,138 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace HotfixMods.Providers.MySql.MySqlConnector.Client
 {
-    public class MySqlClient
+    public partial class MySqlClient
     {
-        string _server;
-        string _user;
-        string _password;
-        string _worldSchemaName;
-        string _charactersSchemaName;
-        string _hotfixesSchemaName;
-
         MySqlConnection _mySqlConnection;
 
-        public MySqlClient(string server, string user, string password, string worldSchemaName, string charactersSchemaName, string hotfixesSchemaName)
+        public MySqlClient(string server, string user, string password)
         {
-            _server = server;
-            _user = user;
-            _password = password;
-            _worldSchemaName = worldSchemaName;
-            _charactersSchemaName = charactersSchemaName;
-            _hotfixesSchemaName = hotfixesSchemaName;
+            _mySqlConnection = new MySqlConnection($"Server={server};User ID={user};Password={password}");
         }
 
-        public async Task<Dictionary<string, object>> RawGetQueryAsync(Dictionary<string, Type> definitions, string tableName, string? whereClause = null)
+        public async Task<IEnumerable<IDictionary<string, object>>> GetAsync(string schemaName, string tableName, Dictionary<string, Type> definitions, string? whereClause = null)
         {
-            var results = new Dictionary<string, object>();
-            var query = $"SELECT ";
+            ValidateInput(tableName, whereClause);
+
+            if (definitions.Count == 0)
+            {
+                throw new Exception("Missing definitions.");
+            }
+
+            var results = new List<Dictionary<string, object>>();
+
+            string columns = "";
             foreach (var definition in definitions)
-                query += $"{definitions},";
-            query = query.Remove(query.Length - 1, 1);
-            query += $" FROM {tableName}";
-            if (!string.IsNullOrEmpty(whereClause))
-                query += $"WHERE {whereClause};";
+            {
+                ValidateInput(definition.Key);
+                columns += $"{definition.Key},";
+            }
+            columns = columns.Remove(columns.Length - 1);
+
+            var query = $"SELECT {columns} FROM {schemaName}.{tableName}";
+            query += string.IsNullOrWhiteSpace(whereClause) ? ";" : $" WHERE {whereClause};";
 
             using var command = new MySqlCommand(query, _mySqlConnection);
             using var reader = await command.ExecuteReaderAsync();
 
-            for (int i = 0; i<definitions.Count; i++)
+            while (reader.Read())
             {
-                string fieldName = definitions.ElementAt(i).Key;
-                object fieldProperty = definitions.ElementAt(i).Value.ToString() switch
+                var result = new Dictionary<string, object>();
+                for (int i = 0; i < definitions.Count; i++)
                 {
-                    "System.Int8" => reader.GetSByte(i),
-                    "System.Int16" => reader.GetInt16(i),
-                    "System.Int32" => reader.GetInt32(i),
-                    "System.Int64" => reader.GetInt64(i),
-                    "System.UInt8" => reader.GetByte(i),
-                    "System.UInt16" => reader.GetUInt16(i),
-                    "System.UInt32" => reader.GetUInt32(i),
-                    "System.UInt64" => reader.GetUInt64(i),
-                    "System.String" => reader.GetString(i),
-                    "System.Decimal" => (decimal)reader.GetFloat(i),
+                    string fieldName = definitions.ElementAt(i).Key;
+                    object fieldType = definitions.ElementAt(i).Value.ToString() switch
+                    {
+                        "System.Int8" => reader.GetSByte(i),
+                        "System.Int16" => reader.GetInt16(i),
+                        "System.Int32" => reader.GetInt32(i),
+                        "System.Int64" => reader.GetInt64(i),
+                        "System.UInt8" => reader.GetByte(i),
+                        "System.UInt16" => reader.GetUInt16(i),
+                        "System.UInt32" => reader.GetUInt32(i),
+                        "System.UInt64" => reader.GetUInt64(i),
+                        "System.String" => reader.GetString(i),
+                        "System.Decimal" => (decimal)reader.GetFloat(i),
+                        _ => throw new Exception("Not implemented")
+                    };
+                    result.Add(fieldName, fieldType);
+                }
+                results.Add(result);
+            }
+
+            return results;
+        }
+
+        public async Task AddOrUpdateAsync(string schemaName, string tableName, Dictionary<string, object> data)
+        {
+            ValidateInput(schemaName, tableName);
+
+            if (data.Count == 0)
+            {
+                throw new Exception("Nothing to add or update.");
+            }
+
+            using var cmd = new MySqlCommand(_mySqlConnection, null);
+            string columns = "";
+            string values = "";
+            for (int i = 0; i < data.Count; i++)
+            {
+                columns += $"{data.ElementAt(i).Key},";
+                values += $"@{data.ElementAt(i).Key},";
+                cmd.Parameters.AddWithValue($"{data.ElementAt(i).Key}", data.ElementAt(i).Value);
+            }
+
+            string query = $"REPLACE INTO {schemaName}.{tableName} ({columns}) VALUES({values});";
+            cmd.CommandText = query;
+            await cmd.ExecuteNonQueryAsync();
+        }
+
+        public async Task DeleteAsync(string schemaName, string tableName, string whereClause)
+        {
+            ValidateInput(schemaName, tableName, whereClause);
+            if (string.IsNullOrWhiteSpace(tableName) || string.IsNullOrWhiteSpace(whereClause))
+                throw new Exception("Parameters can not be null.");
+
+            var query = $"DELETE FROM {schemaName}.{tableName} WHERE {whereClause};";
+            using var cmd = new MySqlCommand(query, _mySqlConnection);
+            await cmd.ExecuteNonQueryAsync();
+        }
+
+        public async Task CreateTableIfNotExistAsync(string schemaName, string tableName, Dictionary<string, Type> definitions)
+        {
+            ValidateInput(schemaName, tableName);
+            string columns = "";
+            for (int i = 0; i < definitions.Count; i++)
+            {
+                var fieldName = definitions.ElementAt(i).Key;
+                var fieldType = definitions.ElementAt(i).Value.ToString() switch
+                {
+                    "System.Int8" => "tinyint signed",
+                    "System.Int16" => "smallint signed",
+                    "System.Int32" => "int signed",
+                    "System.Int64" => "bigint signed",
+                    "System.UInt8" => "tinyint unsigned",
+                    "System.UInt16" => "smallint unsigned",
+                    "System.UInt32" => "int unsigned",
+                    "System.UInt64" => "bigint unsigned",
+                    "System.String" => "text",
+                    "System.Decimal" => "float",
                     _ => throw new Exception("Not implemented")
                 };
-                results.Add(fieldName, fieldProperty);
+                ValidateInput(fieldName);
+                columns += $"{fieldName} {fieldType},";
             }
-            return results;
+
+            string query = $"CREATE TABLE IF NOT EXISTS {schemaName}.{tableName} ({columns} primary key(ID, VerifiedBuild));";
+            using var cmd = new MySqlCommand(query, _mySqlConnection);
+            await cmd.ExecuteNonQueryAsync();
         }
     }
 }

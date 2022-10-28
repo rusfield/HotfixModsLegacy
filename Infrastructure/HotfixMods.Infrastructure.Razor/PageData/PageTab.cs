519536f3f218fe198703dac9dff3e886031edf28
Marius <marius@fjeld.et>
2022-10-28 23:09:18 +0200

    Load fields for generic hotfix

--- Infrastructure/HotfixMods.Infrastructure.Razor/PageData/PageTab.cs
+++ Infrastructure/HotfixMods.Infrastructure.Razor/PageData/PageTab.cs
@@ -13,7 +13,7 @@ namespace HotfixMods.Infrastructure.Razor.PageData
         public string TabName { get; set; }
         public Guid TabId { get; }
         public object? Dto { get; set; }
-        public object? LookupDto { get; set; }
+        public object? DtoLookup { get; set; }
         public Type PageType { get; set; }
 
     }

2fcff2389f54ebb9ce82dfffd9d472727ea85912
Marius <marius@fjeld.et>
2022-11-13 19:25:39 +0100

    CreatureModelInfo tab

--- Core/HotfixMods.Core/Models/TrinityCore/CreatureModelInfo.cs
+++ Core/HotfixMods.Core/Models/TrinityCore/CreatureModelInfo.cs
@@ -8,7 +8,7 @@ namespace HotfixMods.Core.Models.TrinityCore
         public uint DisplayId { get; set; } = 0;
         public decimal BoundingRadius { get; set; } = 0;
         public decimal CombatReach { get; set; } = 0;
-        public uint DisplayID_Other_Gender { get; set; } = 0;
+        public uint DisplayId_Other_Gender { get; set; } = 0;
         public int VerifiedBuild { get; set; } = -1;
     }
 
---
+++ Infrastructure/HotfixMods.Infrastructure/InfoModels/CreatureModelInfoInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace HotfixMods.Infrastructure.InfoModels
+{
+    public class CreatureModelInfoInfo
+    {
+        public static string BoundingRadius = "TODO";
+        public static string CombatReach = "TODO";
+        public static string DisplayId_Other_Gender = "The Display ID of the creature as the other gender. To utilize this for a custom creature in HotfixMods, create a new creature in the different gender and link them to each other here.\r\nFor more advanced configurations on multiple display models on the same creature, edit in TrinityCore directly.";
+    }
+}

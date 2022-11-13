81472a33b0285fd98163402d37334c905be2f9cd
Marius <marius@fjeld.et>
2022-11-13 13:39:08 +0100

    Spell and Creature DTO

--- Infrastructure/HotfixMods.Infrastructure/DtoModels/CreatureDto.cs
+++ Infrastructure/HotfixMods.Infrastructure/DtoModels/CreatureDto.cs
@@ -1,7 +1,20 @@
-﻿namespace HotfixMods.Infrastructure.DtoModels
+﻿using HotfixMods.Core.Models.Db2;
+using HotfixMods.Core.Models.TrinityCore;
+
+namespace HotfixMods.Infrastructure.DtoModels
 {
     public class CreatureDto : BaseDto
     {
         public CreatureDto() : base("Creature") { }
+
+        public CreatureTemplate CreatureTemplate { get; set; } = new();
+        public CreatureTemplateAddon CreatureTemplateAddon { get; set; } = new();
+        public CreatureTemplateModel CreatureTemplateModel { get; set; } = new();
+        public CreatureDisplayInfo CreatureDisplayInfo { get; set; } = new();
+        public CreatureDisplayInfoExtra CreatureDisplayInfoExtra { get; set; } = new();
+        public CreatureEquipTemplate CreatureEquipTemplate { get; set; } = new();
+        public CreatureModelInfo CreatureModelInfo { get; set; } = new();
+        public List<CreatureDisplayInfoOption> CreatureDisplayInfoOptions { get; set; } = new();
+        public List<NpcModelItemSlotDisplayInfo> NpcModelItemSlotDisplayInfos { get; set; } = new();
     }
 }
--- Infrastructure/HotfixMods.Infrastructure/DtoModels/SpellDto.cs
+++ Infrastructure/HotfixMods.Infrastructure/DtoModels/SpellDto.cs
@@ -5,5 +5,17 @@ namespace HotfixMods.Infrastructure.DtoModels
     public class SpellDto : BaseDto
     {
         public SpellDto() : base(nameof(Spell)) { }
+
+        public Spell Spell { get; set; } = new();
+        public SpellAuraOptions SpellAuraOptions { get; set; } = new();
+        public SpellCooldowns SpellCooldowns { get; set; } = new();
+        public SpellMisc SpellMisc { get; set; } = new();
+        public SpellName SpellName { get; set; } = new();
+        public SpellPower SpellPower { get; set; } = new();
+        public List<SpellEffect> SpellEffects { get; set; } = new();
+        public List<SpellXSpellVisual> SpellXSpellVisuals { get; set; } = new();
+
+        public SpellVisual SpellVisual { get; set; } = new();
+        public SpellVisualEvent SpellVisualEvent { get; set; } = new();
     }
 }

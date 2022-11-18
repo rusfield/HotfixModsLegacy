ae077f2a41cb5ec325430748cc7a8cae6feffbac
Marius <marius@fjeld.et>
2022-11-18 13:18:38 +0100

    Fix transitions

---
+++ Infrastructure/HotfixMods.Infrastructure.Razor/Handlers/TransitionHandler.cs
@@ -0,0 +1,9 @@
+﻿using MudBlazor;
+
+namespace HotfixMods.Infrastructure.Razor.Handlers
+{
+    public static class TransitionHandler
+    {
+        public static Transition Transition { get; set; } = Transition.None;
+    }
+}

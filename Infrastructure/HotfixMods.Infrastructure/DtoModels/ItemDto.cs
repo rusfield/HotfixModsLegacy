74d89bbba4573589c5dab8652892af2064188cec
Marius <marius@fjeld.et>
2022-11-23 15:10:12 +0100

    Decrease tab name

--- Infrastructure/HotfixMods.Infrastructure/DtoModels/ItemDto.cs
+++ Infrastructure/HotfixMods.Infrastructure/DtoModels/ItemDto.cs
@@ -10,7 +10,7 @@ namespace HotfixMods.Infrastructure.DtoModels
         public Item Item { get; set; } = new();
         public ItemAppearance ItemAppearance { get; set; } = new();
         public ItemDisplayInfo ItemDisplayInfo { get; set; } = new();
-        public ItemModifiedAppearance ItemMItemModifiedAppearance { get; set; } = new();
+        public ItemModifiedAppearance ItemModifiedAppearance { get; set; } = new();
         public ItemSearchName ItemSearchName { get; set; } = new();
         public ItemSparse ItemSparse { get; set; } = new();
         public List<ItemDisplayInfoMaterialRes> ItemDisplayInfoMaterialRes { get; set; } = new();

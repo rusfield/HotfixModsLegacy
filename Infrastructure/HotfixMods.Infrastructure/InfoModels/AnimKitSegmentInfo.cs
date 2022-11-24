2cfb1c7f24aec0b8e1f6460cf773150684d3f223
Marius <marius@fjeld.et>
2022-11-24 10:03:23 +0100

    Fix disabled adornments

--- Infrastructure/HotfixMods.Infrastructure/InfoModels/AnimKitSegmentInfo.cs
+++ Infrastructure/HotfixMods.Infrastructure/InfoModels/AnimKitSegmentInfo.cs
@@ -2,7 +2,7 @@
 {
     public static class AnimKitSegmentInfo
     {
-        public static string OrderIndex = "Order Index works as an ID for a group of segments linked to an AnimKit.\nThe order does not always matter, but can do based on the condition values.";
+        public static string OrderIndex = "Order Index works as an ID for a group of segments linked to an AnimKit.\nThe order does not always matter, but can do based on the condition values.\nTo edit this value, move the segment using the controllers in the footer.";
         public static string AnimId = "The ID of animation to play for this segment.\nNote that the animation will only work if the creature that plays it supports this animation, and will play nothing or a fallback animation if not.\nUse a tool like WoW Model Viewer to see what animations are available to a creature or a player character.";
         public static string AnimStartTime = "The frame to start play the animation from.\nUse a tool like WoW Model Viewer to play an animation and find the specific frame you want to start play from, if not from the beginning (which is 0).";
         public static string AnimKitConfigId = "Maps to the AnimKitConfig record to use for this segment. AnimKitConfig and it's referencing tables holds values such as which bodyparts will play the given animation and with what priority.";

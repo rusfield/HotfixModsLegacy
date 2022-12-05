fc554c2fccdc0f7b2fa37db2d893f2cc3bcf5f03
Marius <marius@fjeld.et>
2022-12-05 13:48:37 +0100

    Enums for priority and bone set

--- Infrastructure/HotfixMods.Infrastructure/InfoModels/AnimKitConfigBoneSetInfo.cs
+++ Infrastructure/HotfixMods.Infrastructure/InfoModels/AnimKitConfigBoneSetInfo.cs
@@ -3,6 +3,6 @@
     public static class AnimKitConfigBoneSetInfo
     {
         public static string AnimKitBoneSetId { get; set; } = "The ID of the bone set that the current segment will be played on.\r\nAll bone sets except Full Body has a parent, so this property can be used to isolate an animation on a specific body part. Remember to set Priority accordingly.";
-        public static string AnimKitPriorityId { get; set; } = "The ID of the priority that the current segment will be played with.\r\nWith multiple segments, the client needs to know in what priority the bone set in the current segment shall be played on.";
+        public static string AnimKitPriorityId { get; set; } = "The ID of the priority that the current segment will be played with.\r\nWith multiple segments, the client needs to know in what priority the bone set in the current segment shall be played on.\r\n\r\nThe higher the number, the higher the priority.";
     }
 }

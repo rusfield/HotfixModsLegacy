e13938cb36cb7388e3f134249fadde84d1fcd662
Marius <marius@fjeld.et>
2022-12-16 12:51:23 +0100

    Shared entity tab

---
+++ Infrastructure/HotfixMods.Infrastructure/InfoModels/HotfixModsEntityInfo.cs
@@ -0,0 +1,9 @@
+﻿
+namespace HotfixMods.Infrastructure.InfoModels
+{
+    public static class HotfixModsEntityInfo
+    {
+        public static string Id = "Choose whether you want to save a new or update an existing entity based on ID.\r\n\r\nAn existing entity can at any time be saved as a new entity, however due to all the foreign keys an entity can only be updated if opened directly.";
+
+    }
+}

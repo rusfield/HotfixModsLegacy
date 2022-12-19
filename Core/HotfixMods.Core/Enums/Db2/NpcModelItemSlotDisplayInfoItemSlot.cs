a1560f37b8f91447418c533a3093f06159af9166
Marius <marius@fjeld.et>
2022-12-19 22:38:04 +0100

    Creature fixes

---
+++ Core/HotfixMods.Core/Enums/Db2/NpcModelItemSlotDisplayInfoItemSlot.cs
@@ -0,0 +1,18 @@
+﻿namespace HotfixMods.Core.Enums.Db2
+{
+    public enum NpcModelItemSlotDisplayInfoItemSlot
+    {
+        HEAD = 0,
+        SHOULDERS = 1,
+        SHIRT = 2,
+        CHEST = 3,
+        WAIST = 4,
+        LEGS = 5,
+        FEET = 6,
+        WRISTS = 7,
+        HANDS = 8,
+        TABARD = 9,
+        CAPE = 10,
+        QUIVER = 11
+    }
+}

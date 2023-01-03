7694b6fe76e9ee0bb11ad3fba4319079380cf17f
Marius <marius@fjeld.et>
2023-01-03 14:02:27 +0100

    Implementations

--- Infrastructure/HotfixMods.Infrastructure.Blazor/Components/DtoContent/PagedDtoContentBase.razor.cs
+++ Infrastructure/HotfixMods.Infrastructure.Blazor/Components/DtoContent/PagedDtoContentBase.razor.cs
@@ -6,6 +6,7 @@ namespace HotfixMods.Infrastructure.Blazor.Components.DtoContent
 {
     public partial class PagedDtoContentBase<TDto, TValue> : ComponentBase
         where TDto : IDto
+        where TValue : class
     {
         [CascadingParameter(Name = "Dto")]
         public IDto Dto { get; set; }
@@ -14,14 +15,29 @@ namespace HotfixMods.Infrastructure.Blazor.Components.DtoContent
 
         public Transition Transition { get; set; }
         public TValue Value { get; set; }
-        public TValue ValueCompare { get; set; }
+        public TValue? ValueCompare { get; set; }
 
         protected override void OnParametersSet()
         {
-            var type = Dto.GetType();
-            var property = type.GetProperty(typeof(TValue).Name);
-            Value = (TValue)property.GetValue(Dto);
+            var dtoType = Dto.GetType();
+            var dtoProperty = dtoType.GetProperty(typeof(TValue).Name);
+            Value = (TValue)dtoProperty.GetValue(Dto);
             base.OnParametersSet();
+        }
+
+        protected override void OnAfterRender(bool firstRender)
+        {
+            if(DtoCompare != null)
+            {
+                var dtoCompareType = DtoCompare.GetType();
+                var dtoCompareProperty = dtoCompareType.GetProperty(typeof(TValue).Name);
+                ValueCompare = (TValue)dtoCompareProperty.GetValue(DtoCompare);
+            }
+            else
+            {
+                ValueCompare = null;
+            }
+            base.OnAfterRender(firstRender);
         }
     }
 }

e79af4f2e851af6ae62671ee92a4a7e244e37a8c
Marius <marius@fjeld.et>
2023-01-11 15:32:58 +0100

    Attempt on solving nullable Values

---
+++ Infrastructure/HotfixMods.Infrastructure.Blazor/Components/DtoContent/StandardDtoContent.razor.cs
@@ -0,0 +1,35 @@
+﻿using HotfixMods.Infrastructure.Blazor.PageData;
+using HotfixMods.Infrastructure.Extensions;
+using Microsoft.AspNetCore.Components;
+
+namespace HotfixMods.Infrastructure.Blazor.Components.DtoContent
+{
+    public partial class StandardDtoContent<T> : ComponentBase
+        where T : class, new()
+    {
+        [CascadingParameter(Name = "PageTab")]
+        public PageTab PageTab { get; set; }
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+        bool valueIsNull = true;
+
+        protected override void OnAfterRender(bool firstRender)
+        {
+            valueIsNull = null == PageTab?.Dto?.GetDtoValue<T>();
+            base.OnAfterRender(firstRender);
+        }
+
+        protected override void OnParametersSet()
+        {
+            valueIsNull = null == PageTab?.Dto?.GetDtoValue<T>();
+            base.OnParametersSet();
+        }
+
+        void CreateValue()
+        {
+            PageTab.Dto.SetDtoValueToDefault<T>();
+            valueIsNull = false;
+        }
+    }
+}

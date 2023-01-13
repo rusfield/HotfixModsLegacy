cdcb6dfc8ed6ba410f4ba07c3782885ebea973e3
Marius <marius@fjeld.et>
2023-01-13 13:20:01 +0100

    HotfixModsEntity implementation

---
+++ Infrastructure/HotfixMods.Infrastructure.Blazor/Pages/Shared/HotfixModsEntity_Tab.razor.cs
@@ -0,0 +1,18 @@
+﻿using HotfixMods.Core.Models.TrinityCore;
+using HotfixMods.Infrastructure.Blazor.Components.DtoContent;
+using HotfixMods.Infrastructure.DtoModels;
+
+namespace HotfixMods.Infrastructure.Blazor.Pages.Shared
+{
+    public partial class HotfixModsEntity_Tab<TDto> : DtoContentBase<TDto, HotfixModsEntity>
+        where TDto : IDto
+    {
+        async Task<Dictionary<bool, string>> GetOptions()
+        {
+            return new() {
+            { true, $"{Value.RecordId} (update existing)" },
+            { false, "New (generated after save)"}
+        };
+        }
+    }
+}
--- Infrastructure/HotfixMods.Infrastructure/DtoModels/IDto.cs
+++ Infrastructure/HotfixMods.Infrastructure/DtoModels/IDto.cs
@@ -1,7 +1,11 @@
-﻿namespace HotfixMods.Infrastructure.DtoModels
+﻿using HotfixMods.Core.Models.TrinityCore;
+
+namespace HotfixMods.Infrastructure.DtoModels
 {
     public interface IDto
     {
+        public HotfixModsEntity HotfixModsEntity { get; set; }
+        public bool IsUpdate { get; set; }
         public void AddToGroup(Type groupType, int index);
         public void RemoveFromGroup(Type groupType, int index);
         public void MoveInGroup(Type groupType, int oldIndex, int newIndex);

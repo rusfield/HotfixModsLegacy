c37ab7cf6b5a642131a86b7a217c703d2607469c
Marius <marius@fjeld.et>
2023-01-18 15:32:10 +0100

    Item page

--- Core/HotfixMods.Core/Enums/Db2/ItemDisplayInfoMaterialResComponentSection.cs
+++ Core/HotfixMods.Core/Enums/Db2/ItemDisplayInfoMaterialResComponentSection.cs
@@ -1,24 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HotfixMods.Core.Enums.Db2
+﻿namespace HotfixMods.Core.Enums.Db2
 {
     public enum ItemDisplayInfoMaterialResComponentSection
     {
-        ARMUPPER = 0,
-        ARMLOWER = 1,
+        ARM_UPPER = 0,
+        ARM_LOWER = 1,
         HAND = 2,
-        TORSOUPPER = 3,
-        TORSOLOWER = 4,
-        LEGUPPER = 5,
-        LEGLOWER = 6,
+        TORSO_UPPER = 3,
+        TORSO_LOWER = 4,
+        LEG_UPPER = 5,
+        LEG_LOWER = 6,
         FOOT = 7,
         ACCESSORY = 8,
-        SCALPUPPER = 9,
-        SCALPLOWER = 10,
+        SCALP_UPPER = 9,
+        SCALP_LOWER = 10,
         UNK_11 = 11,
         TATTOO_UNK0 = 12,
         TATTOO_UNK1 = 13,

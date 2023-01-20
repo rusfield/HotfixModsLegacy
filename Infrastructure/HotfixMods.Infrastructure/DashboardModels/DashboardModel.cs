b4bebebd27930379709ba08e246ca954b0f9f834
Marius <marius@fjeld.et>
2023-01-20 13:40:13 +0100

    Progress on dashboard

--- Infrastructure/HotfixMods.Infrastructure/DashboardModels/DashboardModel.cs
+++ Infrastructure/HotfixMods.Infrastructure/DashboardModels/DashboardModel.cs
@@ -3,6 +3,7 @@
     public class DashboardModel
     {
         public int Id { get; set; }
+        public int? AdditionalId { get; set; } // For example ItemDisplayInfo or CreatureDisplayInfo
         public string? Name { get; set; }
         public string? AvatarUrl { get; set; }
         public string? Comment { get; set; }

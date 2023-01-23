2094fc17bccff506b11b63a5896cd61815921e3b
Marius <marius@fjeld.et>
2023-01-23 15:11:56 +0100

    Cleanups

---
+++ Infrastructure/HotfixMods.Infrastructure.Blazor/Handlers/TabHandler.cs
@@ -0,0 +1,9 @@
+﻿using HotfixMods.Infrastructure.Blazor.PageData;
+
+namespace HotfixMods.Infrastructure.Blazor.Handlers
+{
+    public static class TabHandler
+    {
+        public static Action<PageTab>? LaunchTab;
+    }
+}

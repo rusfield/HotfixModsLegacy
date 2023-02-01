beee7a181cfcdce422a2fb198ddc1bee4088265d
Marius <marius@fjeld.et>
2023-02-01 11:32:04 +0100

    Cleanup in dialogs

--- Apps/HotfixMods.Apps.MauiBlazor/MauiProgram.cs
+++ Apps/HotfixMods.Apps.MauiBlazor/MauiProgram.cs
@@ -117,64 +117,6 @@ namespace HotfixMods.Apps.MauiBlazor
                 };
             });
 
-            /*
-            // TODO: Cleanup 
-            IDb2Provider db2Provider = new Db2Client();
-            //IMySqlProvider mySqlProvider = new MySqlClient("127.0.0.1","root","root","world","characters","hotfixes");
-            IMySqlProvider mySqlProvider = new MySqlProvider.Debug.Clients.MySqlProvider();
-
-            builder.Services.AddSingleton(config =>
-            {
-                return new CreatureService(db2Provider, mySqlProvider)
-                {
-                    VerifiedBuild = -2000,
-                    IdSize = 5000,
-                    IdRangeFrom = 100000000,
-                    IdRangeTo = 200000000
-                };
-            });
-            builder.Services.AddSingleton(config =>
-            {
-                return new SoundKitService(db2Provider, mySqlProvider)
-                {
-                    VerifiedBuild = -800,
-                    IdSize = 20,
-                    IdRangeFrom = 1800000,
-                    IdRangeTo = 1900000
-                };
-            });
-            builder.Services.AddSingleton(config =>
-            {
-                return new AnimKitService(db2Provider, mySqlProvider)
-                {
-                    VerifiedBuild = -700,
-                    IdSize = 50,
-                    IdRangeFrom = 60000,
-                    IdRangeTo = 70000
-                };
-            });
-            builder.Services.AddSingleton(config =>
-            {
-                return new SpellService(db2Provider, mySqlProvider)
-                {
-                    VerifiedBuild = -1600,
-                    IdSize = 50, 
-                    IdRangeFrom = 2000000,
-                    IdRangeTo = 3000000
-                };
-            });
-            builder.Services.AddSingleton(config =>
-            {
-                return new SpellVisualKitService(db2Provider, mySqlProvider)
-                {
-                    VerifiedBuild = -1700,
-                    IdSize = 5, // TODO: Check
-                    IdRangeFrom = 300000,
-                    IdRangeTo = 400000
-                };
-            });
-            */
-
             builder.Services.AddBlazorWebViewDeveloperTools();
             builder.Services.AddLogging(configure =>
             {

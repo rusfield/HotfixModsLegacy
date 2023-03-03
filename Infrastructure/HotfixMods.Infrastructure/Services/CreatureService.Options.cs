fffb4f609eb00ce157f557caaa4584c80dd98394
Marius <marius@fjeld.et>
2023-03-03 10:14:30 +0100

    Add faction and fix autocomplete to only supported values

---
+++ Infrastructure/HotfixMods.Infrastructure/Services/CreatureService.Options.cs
@@ -0,0 +1,18 @@
+﻿using HotfixMods.Core.Models.Db2;
+
+namespace HotfixMods.Infrastructure.Services
+{
+    public partial class CreatureService
+    {
+        public async Task<Dictionary<ushort, string>> GetFactionOptionsAsync()
+        {
+            var results = new Dictionary<ushort, string>();
+            var factions = await GetFromClientOnlyAsync<Faction>();
+            foreach (var faction in factions)
+            {
+                results.Add((ushort)faction.ID, faction.Name);
+            }
+            return results;
+        }
+    }
+}

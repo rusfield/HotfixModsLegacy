5728a2540852870bd5c884af9a7df8de765c9b59
Marius <marius@fjeld.et>
2023-03-07 15:28:21 +0100

    Progress on Trinity Reader

--- Providers/HotfixMods.Providers.TrinityCore/Client/TrinityCoreClient.Reader.cs
+++ Providers/HotfixMods.Providers.TrinityCore/Client/TrinityCoreClient.Reader.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using HotfixMods.Core.Models;
+using System.Reflection;
 using System.Text.RegularExpressions;
 
 namespace HotfixMods.Providers.TrinityCore.Client
@@ -49,11 +50,11 @@ namespace HotfixMods.Providers.TrinityCore.Client
                                         {
                                             "FT_STRING" => (typeof(string), true),
                                             "FT_STRING_NOT_LOCALIZED" => (typeof(string), false),
-                                            "FT_BYTE" => (isSigned ? typeof(sbyte) : typeof(byte), i == indexField || i == parentIndexField),
-                                            "FT_SHORT" => (isSigned ? typeof(short) : typeof(ushort), i == indexField || i == parentIndexField),
-                                            "FT_INT" => (isSigned ? typeof(int) : typeof(uint), i == indexField || i == parentIndexField),
-                                            "FT_LONG" => (isSigned ? typeof(long) : typeof(ulong), i == indexField || i == parentIndexField),
-                                            "FT_FLOAT" => (typeof(decimal), i == indexField || i == parentIndexField),
+                                            "FT_BYTE" => (isSigned ? typeof(sbyte) : typeof(byte), i == parentIndexField),
+                                            "FT_SHORT" => (isSigned ? typeof(short) : typeof(ushort), i == parentIndexField),
+                                            "FT_INT" => (isSigned ? typeof(int) : typeof(uint), i == parentIndexField),
+                                            "FT_LONG" => (isSigned ? typeof(long) : typeof(ulong), i == parentIndexField),
+                                            "FT_FLOAT" => (typeof(decimal), i == parentIndexField),
                                             _ => throw new Exception($"Unhandled type: {tcType}.")
                                         });
                                 }
@@ -104,6 +105,80 @@ namespace HotfixMods.Providers.TrinityCore.Client
                 }
             }
             throw new Exception($"DB2MEtadata.h not found.");
+        }
+
+        public string GenerateDb2StoresH(string db2Name)
+        {
+            return $"TC_GAME_API extern DB2Storage<{db2Name}Entry> s{db2Name}Store;";
+        }
+
+        public string GenerateDb2StoresCpp1(string db2Name)
+        {
+            return $"DB2Storage<{db2Name}Entry> s{db2Name}Store(\"{db2Name}.db2\", &{db2Name}LoadInfo::Instance);";
+        }
+
+        public string GenerateDb2StoresCpp2(string db2Name)
+        {
+            return $"LOAD_DB2(s{db2Name}Store);";
+        }
+
+        public string GenerateDb2StructureH(string db2Name, List<(Type, bool)> fields, DbRowDefinition definition)
+        {
+            if(fields.Count != definition.ColumnDefinitions.Count)
+            {
+                throw new Exception($"Unable to generate Db2Structure. Fields and Definitions are not the same length.");
+            }
+            string output = "";
+            output += $"struct {db2Name}Entry\r\n";
+            output += "{\r\n";
+            for(int i = 0; i < fields.Count; i++)
+            {
+                var field = fields[i];
+                var defName = definition.ColumnDefinitions[i].Name;
+                output += $"\t{GetDb2StructType(field.Item1, field.Item2)} {defName};\r\n";
+            }
+            output += "};\r\n";
+            return output;
+        }
+
+        string GetDb2StructType(Type type, bool isTrue)
+        {
+            // isTrue is true for LocalizedStrings or ParentIndexFields
+            if (isTrue)
+            {
+                // Force unsigned or localized
+                return type.ToString() switch
+                {
+                    "System.SByte" => "uint8",
+                    "System.Int16" => "uint16",
+                    "System.Int32" => "uint32",
+                    "System.Int64" => "uint64",
+                    "System.Byte" => "uint8",
+                    "System.UInt16" => "uint16",
+                    "System.UInt32" => "uint32",
+                    "System.UInt64" => "uint64",
+                    "System.String" => "LocalizedString",
+                    "System.Decimal" => "float",
+                    _ => throw new Exception($"Unable to get Db2Structure Type for {type.ToString()},")
+                };
+            }
+            else
+            {
+                return type.ToString() switch
+                {
+                    "System.SByte" => "int8",
+                    "System.Int16" => "int16",
+                    "System.Int32" => "int32",
+                    "System.Int64" => "int64",
+                    "System.Byte" => "uint8",
+                    "System.UInt16" => "uint16",
+                    "System.UInt32" => "uint32",
+                    "System.UInt64" => "uint64",
+                    "System.String" => "LocalizedString",
+                    "System.Decimal" => "float",
+                    _ => throw new Exception($"Unable to get Db2Structure Type for {type.ToString()},")
+                };
+            }
         }
     }
 }

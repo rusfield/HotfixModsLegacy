09bf6085c7571be109c654f5a60624b2217f2c76
Marius <marius@fjeld.et>
2023-05-02 16:13:24 +0200

    Add some more itempreview

---
+++ Infrastructure/HotfixMods.Infrastructure/Extensions/IntExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace HotfixMods.Infrastructure.Extensions
+{
+    public static class IntExtensions
+    {
+        /// <summary>
+        /// Adds a + or - to the number, depending on whether it is positive or negative. If 0, nothing is added.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static string ToStringWithSign(this int value)
+        {
+            if (value <= 0)
+                return value.ToString();
+            return $"+{value}";
+        }
+    }
+}

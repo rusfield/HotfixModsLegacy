9f06e6e606db60ab1f5b54f949fad162b1ffd254
Marius Fjeldet <mariusfjeldet@gmail.com>
2026-04-18 15:45:23 +0200

    Upgrade solution to .NET 10

--- Apps/HotfixMods.Apps.MauiBlazor/App.xaml.cs
+++ Apps/HotfixMods.Apps.MauiBlazor/App.xaml.cs
@@ -5,22 +5,18 @@
         public App()
         {
             InitializeComponent();
-
-            MainPage = new MainPage();
         }
-        
+
         protected override Window CreateWindow(IActivationState activationState)
         {
-            var window = base.CreateWindow(activationState);
-
             const int newWidth = 1920;
             const int newHeight = 1020;
 
-            window.Width = newWidth;
-            window.Height = newHeight;
-
-            return window;
+            return new Window(new MainPage())
+            {
+                Width = newWidth,
+                Height = newHeight
+            };
         }
-        
     }
 }
